960fa1b7ebe49db31bc3ac4330ada421e105f13d
Davi Andrade <amanciodavi2007@gmail.com>
2025-09-18 08:44:57 -0300

    feat(#00-3): Add pom.xml

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.dottec.pdi.project.pdi {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.dottec.pdi.project.pdi to javafx.fxml;
+    exports com.dottec.pdi.project.pdi;
+}
