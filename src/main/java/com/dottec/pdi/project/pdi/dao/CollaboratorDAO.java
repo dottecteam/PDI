763165b0ef009c74b8288a8c8d77e31381e726b8
João Paulo <jopaulo.as8@gmail.com>
2025-09-19 08:36:42 -0300

    feat(#01-2): Add CollaboratorDAO

--- src/main/java/com/dottec/pdi/project/pdi/dao/CollaboratorDAO.java
+++ src/main/java/com/dottec/pdi/project/pdi/dao/CollaboratorDAO.java
@@ -1,5 +1,39 @@
 package com.dottec.pdi.project.pdi.dao;
 
+import com.dottec.pdi.project.pdi.config.Database;
+import com.dottec.pdi.project.pdi.model.Collaborator;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CollaboratorDAO {
 
+    public void save(Collaborator collaborator){
+        Database db = null;
+        try {
+            db = new Database();
+            Connection connection = db.getConnection();
+
+            String sql = "INSERT INTO collaborators (" +
+                    "col_name, " +
+                    "col_cpf, " +
+                    "col_email, " +
+                    "col_status, " +
+                    "col_created_at, " +
+                    "department_id) " +
+                    "VALUES (?, ?, ?, ?, ?, ?);";
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, collaborator.getName());
+            stmt.setString(2, collaborator.getCpf());
+            stmt.setString(3, collaborator.getEmail());
+            stmt.setString(4, collaborator.getStatus().toString());
+            stmt.setInt(5, collaborator.getDepartment());
+
+            System.out.println("Adicionado no Banco!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
