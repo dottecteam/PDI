8bb65c29c243b33c17153240bffac1be0e5090ff
Slot148 <nicolasfreitas2004sp@gmail.com>
2025-09-19 13:56:53 -0300

    refactor(#03-3): Restructured GoalValidator

--- src/main/java/validations/GoalValidator.java
+++ src/main/java/validations/GoalValidator.java
@@ -4,16 +4,12 @@ import com.dottec.pdi.project.pdi.model.Goal;
 
 public class GoalValidator{
 	public boolean goalValidator(Goal goal) {
-		if(goal == null) {
+		if(goal == null /*|| goal.getStatus == null*/ ) {							//Remover comentarios caso necessario verificar o status
 			return false;
 		}
-
-		boolean deadlineDateValid = StringValidator.dateValidate(goal.getDeadline());					//Ja funciona corretamente
-		boolean nameValid = StringValidator.descriptionValidate(goal.getName()); 						//usei o descriptionValidate por não ser um nome próprio
-		boolean descriptionValid = StringValidator.descriptionValidate(goal.getDescription());			//Ja funciona corretamente(de acordo com o pattern)
-		boolean categoryValid = StringValidator.categoryValidate(goal.getCategory());					//Ajustar a função categoryValidate depois
-		
-		
-		return deadlineDateValid && nameValid && descriptionValid && categoryValid; 
+		return	StringValidator.dateValidate(goal.getDeadline()) &&					//Ja funciona corretamente
+				StringValidator.descriptionValidate(goal.getName()) &&				//usei o descriptionValidate por não ser um nome próprio
+				StringValidator.descriptionValidate(goal.getDescription()) &&		//Ja funciona corretamente(de acordo com o pattern)
+				StringValidator.categoryValidate(goal.getCategory());				//Ajustar a função categoryValidate depois 
 	}
 }
