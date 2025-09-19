b2021a7aece73ae99771b65f8dfbd1575bf9b045
kauandomingues <kauandomingues931@gmail.com>
2025-09-19 15:29:00 -0300

    feat(#01-1): Add initial register collaborator form view

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.dottec.pdi.project.pdi {
     requires javafx.fxml;
     requires java.sql;
+    requires io.github.cdimascio.dotenv.java;
     requires javafx.graphics;
     requires javafx.controls;
 
