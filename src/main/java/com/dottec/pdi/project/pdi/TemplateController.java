e13d5dff641285b739c83dd219ae5d0fc61405c0
CauãCursino <cmfcursino@gmail.com>
2025-09-22 14:14:17 -0300

    feat(#00-3): New method to switch between the pages

--- src/main/java/com/dottec/pdi/project/pdi/TemplateController.java
+++ src/main/java/com/dottec/pdi/project/pdi/TemplateController.java
@@ -5,21 +5,42 @@
     import javafx.fxml.Initializable;
     import javafx.scene.control.Label;
     import javafx.scene.layout.AnchorPane;
-    import javafx.scene.Node;
     import javafx.scene.Parent;
-    import javafx.scene.Scene;
     import javafx.scene.input.MouseEvent;
     import javafx.scene.layout.BorderPane;
-    import javafx.stage.Stage;
+
     import java.io.IOException;
     import java.net.URL;
     import java.util.ResourceBundle;
 
-    public class HelloController implements Initializable {
+    public class TemplateController implements Initializable {
 
-        @FXML //Declara os ID's criados
+        //Declara os ID's criados
+
+        //AnchorPane
+        @FXML
         private AnchorPane leftMenu;
 
+        @FXML
+        private AnchorPane tmpCenter;
+
+        @FXML
+        private AnchorPane menuDashboard;
+
+        @FXML
+        private AnchorPane menuCollaborators;
+
+        @FXML
+        private AnchorPane menuModels;
+
+        @FXML
+        private AnchorPane menuSettings;
+
+        @FXML
+        private AnchorPane menuProfile;
+
+
+        //Label
         @FXML
         private Label labelArrow;
 
@@ -35,18 +56,48 @@
         @FXML
         private Label labelProfile;
 
-        @FXML
-        private AnchorPane tmpCenter;
 
+        //BorderPane
         @FXML
         private BorderPane mainPane;
 
 
+
+        //Menu
+
+        //Define a página que inicializa com o projeto
         @Override
         public void initialize(URL url, ResourceBundle resourceBundle) {
             carregarPagina("Goal.fxml");
         }
 
+        //Define os métodos que "chamam" as páginas para a tela
+        @FXML
+        void goToDashboard(MouseEvent event) {
+            carregarPagina("Dashboard.fxml");
+        }
+
+        @FXML
+        void goToCollaborators(MouseEvent event) {
+            carregarPagina("Collaborators.fxml");
+        }
+
+        @FXML
+        void goToModels(MouseEvent event) {
+            carregarPagina("Models.fxml");
+        }
+
+        @FXML
+        void goToSettings(MouseEvent event) {
+            carregarPagina("Settings.fxml");
+        }
+
+        @FXML
+        void goToProfile(MouseEvent event) {
+            carregarPagina("Profile.fxml");
+        }
+
+        //Méeodo carregarPagina
         private void carregarPagina(String nomePagina) {
             Parent root = null;
             try{
