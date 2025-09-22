5fd6546c690ce2a12d2a8ee4d918bdf1f3349ac5
CauãCursino <cmfcursino@gmail.com>
2025-09-22 14:14:17 -0300

    feat(#00-3): New method to switch between the pages

--- src/main/java/com/dottec/pdi/project/pdi/Application.java
+++ src/main/java/com/dottec/pdi/project/pdi/Application.java
@@ -1,6 +1,5 @@
 package com.dottec.pdi.project.pdi;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,10 +10,10 @@ import javafx.stage.Screen;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/Template.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("views/Template.fxml"));
         Parent root = fxmlLoader.load();
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         Scene scene = new Scene(root, screenBounds.getWidth(), screenBounds.getHeight());
