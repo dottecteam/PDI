4d2f14c5dc6d8452e56ab5b40b09193b5ee80b32
kauandomingues <kauandomingues931@gmail.com>
2025-09-22 11:39:05 -0300

    chore(#01-05): Add style register collaborator forms

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module com.dottec.pdi.project.pdi {
 
 
     opens com.dottec.pdi.project.pdi to javafx.fxml;
+    opens com.dottec.pdi.project.pdi.controllers;
 
     exports com.dottec.pdi.project.pdi;
+    exports com.dottec.pdi.project.pdi.controllers;
 }
