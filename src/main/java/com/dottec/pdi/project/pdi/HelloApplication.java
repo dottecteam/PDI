5f364c5e087dab1188d20e4e814120dd2bb49daa
JessicaKatayama <jessica.katayama123@gmail.com>
2025-09-24 08:24:58 -0300

    feat: adiciona tela para visualizacao de metas do usuário

--- src/main/java/com/dottec/pdi/project/pdi/HelloApplication.java
+++ src/main/java/com/dottec/pdi/project/pdi/HelloApplication.java
@@ -14,12 +14,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/Template.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/dottec/pdi/project/pdi/views/AddGoal.fxml"));
         Parent root = fxmlLoader.load();
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         Scene scene = new Scene(root, screenBounds.getWidth(), screenBounds.getHeight());
 
-        
+        scene.getStylesheets().add(getClass().getResource("/com/dottec/pdi/project/pdi/static/style/style_AddGoal.css").toExternalForm());
+
         stage.setTitle("PDI");
         stage.setScene(scene);
         stage.setMaximized(true);
