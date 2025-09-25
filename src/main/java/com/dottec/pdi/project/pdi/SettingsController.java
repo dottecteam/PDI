3cc72daa9b06bcbc26b9f679d2f4a0e9f9e6b488
CauãCursino <cmfcursino@gmail.com>
2025-09-25 10:03:17 -0300

    feat(#00-3): Add 'Parent controller'

--- src/main/java/com/dottec/pdi/project/pdi/SettingsController.java
+++ src/main/java/com/dottec/pdi/project/pdi/SettingsController.java
@@ -1,4 +1,24 @@
 package com.dottec.pdi.project.pdi;
 
-public class SettingsController {
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class SettingsController implements ParentController{
+
+    private TemplateController TemplateController;
+
+    @Override
+    public void setTemplateController(TemplateController controller) {
+        this.TemplateController = controller;
+    }
+
+    @FXML
+    void goToTeste(MouseEvent event) {
+        if (TemplateController != null) {
+            TemplateController.carregarPagina("Dashboard.fxml");
+        } else {
+            System.err.println("TemplateController não foi injetado!");
+        }
+    }
 }
