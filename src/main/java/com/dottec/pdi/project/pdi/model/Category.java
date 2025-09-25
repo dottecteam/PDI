04f5301a57257c2e5cea610e9608c902c2ec462e
Davi Andrade <amanciodavi2007@gmail.com>
2025-09-25 08:42:16 -0300

    fix(#05-1): categoryDAO fix bugs

--- src/main/java/com/dottec/pdi/project/pdi/model/Category.java
+++ src/main/java/com/dottec/pdi/project/pdi/model/Category.java
@@ -1,4 +1,64 @@
 package com.dottec.pdi.project.pdi.model;
 
+import com.dottec.pdi.project.pdi.model.enums.CategoryType;
+import java.util.Objects;
+
 public class Category {
+
+    private int id;
+    private String name;
+    private CategoryType type;
+
+    public Category() {}
+
+    public Category(int id, String name, CategoryType type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoryType getType() {
+        return type;
+    }
+
+    public void setType(CategoryType type) {
+        this.type = type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Category other = (Category) obj;
+        return id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
