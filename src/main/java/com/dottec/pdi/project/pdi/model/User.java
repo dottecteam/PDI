50bd11b0172bf2a08579c4ced3f17605d2e2816b
CauãCursino <cmfcursino@gmail.com>
2025-10-13 16:53:27 -0300

    feat(#10-1): Login Controller implementation and User model

---
+++ src/main/java/com/dottec/pdi/project/pdi/model/User.java
@@ -0,0 +1,97 @@
+package com.dottec.pdi.project.pdi.model;
+
+import com.dottec.pdi.project.pdi.enums.Status;
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private Status status;
+
+
+    public User(int id, String name, String email, String password, String role, Status status) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Status getStatus(){
+        return status;
+    }
+
+    public void setStatus(Status status){
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, password);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User other = (User) obj;
+        return id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
