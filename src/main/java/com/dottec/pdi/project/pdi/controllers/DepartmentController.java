acc2f1c90b8564cbcb826d0c289831c8bdb1e080
Breno Reis <reisb6774@gmail.com>
2025-10-13 10:35:36 -0300

    feat(#07-3):Introducing the DepartmentController class

---
+++ src/main/java/com/dottec/pdi/project/pdi/controllers/DepartmentController.java
@@ -0,0 +1,83 @@
+// --------------- Creating the DepartmentController script ---------- //
+
+package com.dottec.pdi.project.pdi.controllers;
+
+import com.dottec.pdi.project.pdi.dao.DepartmentDAO;
+import com.dottec.pdi.project.pdi.model.Department;
+
+
+public class DepartmentController {
+
+    private final DepartmentDAO departmentDAO = new DepartmentDAO();
+
+
+    // -------------- Creating the methods for the DepartmentDao Controller class ------------//
+
+    // -------- Adding department ----------- //
+    public boolean addDepartment( Department department){
+        Department existingDepartment = departmentDAO.findById(department.getId());
+
+        if( existingDepartment != null){
+            System.out.println( "Department already exists" );
+            return false;
+        }
+        // ---  If there is no such department ---- //
+
+        try{
+            departmentDAO.insert(department);
+            return true;
+        }catch( Exception e){
+            System.out.println( "Department insert failed. Error message: " + e.getMessage() );
+            return false;
+        }
+
+    }
+
+    // -------- Remove department method ------ //
+    public boolean deleteDepartment( int id){
+        Department existingDepartment = departmentDAO.findById(id);
+
+        if( existingDepartment == null) {
+            System.out.println("Department not found");
+            return false;
+        }
+
+        try {
+            departmentDAO.delete(id); // Deleting in the Data-Base
+            return true;
+        }catch ( Exception e ) {
+            System.out.println("Department delete failed. Error message: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // -------- Update department --------- //
+    public boolean  updateDepartment( Department department){
+        Department existingDepartment = departmentDAO.findById(department.getId());
+
+        if( existingDepartment == null){
+            System.out.println("Department not found");
+            return false;
+        }
+
+        try{
+            departmentDAO.update(department);
+            return true;
+        }catch( Exception e){
+            System.out.println( "Department update failed. Error message: " + e.getMessage() );
+            return false;
+        }
+
+    }
+
+    // ---------- Getting Department per Id -------//
+
+    public Department findById(int id){
+        Department existingDepartment = departmentDAO.findById(id);
+        if( existingDepartment == null){
+            System.out.println("Department not found");
+        }
+        return existingDepartment;
+
+    }
+}
