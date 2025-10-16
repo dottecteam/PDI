2814cc03c7c54c200f3380a46ed16faf61e35dc1
CauãCursino <cmfcursino@gmail.com>
2025-10-16 09:09:26 -0300

    feat(#10-1): Auth Controller implementation

---
+++ src/main/java/com/dottec/pdi/project/pdi/controllers/AuthController.java
@@ -0,0 +1,68 @@
+package com.dottec.pdi.project.pdi.controllers;
+
+import com.dottec.pdi.project.pdi.config.Database;
+import com.dottec.pdi.project.pdi.model.enums.Status;
+import com.dottec.pdi.project.pdi.model.User;
+import com.dottec.pdi.project.pdi.model.enums.Role;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AuthController {
+
+    //Guarda o usuario
+    private static User loggedUser;
+
+    //Retorna o usuário atualmente logado
+    public static User getLoggedUser() {
+        return loggedUser;
+    }
+
+    //Faz login e autentica no banco
+    public static boolean login(String email, String password) {
+        String sql = "SELECT * FROM users WHERE use_email = ? AND use_password_hash = ?";
+
+        try (Connection conn = Database.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+
+            ResultSet login = stmt.executeQuery();
+
+            if (login.next()) {
+                Status status = Status.valueOf(login.getString("use_status").toUpperCase());
+                Role role = Role.valueOf(login.getString("use_role"));
+
+                loggedUser = new User(
+                        login.getInt("use_id"),
+                        login.getString("use_name"),
+                        login.getString("use_email"),
+                        login.getString("use_password_hash"),
+                        login.getInt("department_id"),
+                        role,
+                        status
+                );
+                return true;
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao autenticar usuário: " + e.getMessage());
+        }
+
+        return false;
+    }
+
+    // Faz logout limpando o usuário logado
+    public static void logout() {
+        loggedUser = null;
+    }
+
+    //Verifica se há um usuário logado
+    public static boolean isAuthenticated() {
+        return loggedUser != null;
+    }
+}
--- src/main/java/com/dottec/pdi/project/pdi/model/User.java
+++ src/main/java/com/dottec/pdi/project/pdi/model/User.java
@@ -1,6 +1,8 @@
 package com.dottec.pdi.project.pdi.model;
 
-import com.dottec.pdi.project.pdi.enums.Status;
+import com.dottec.pdi.project.pdi.model.enums.Role;
+import com.dottec.pdi.project.pdi.model.enums.Status;
+
 import java.util.Objects;
 
 public class User {
@@ -8,21 +10,26 @@ public class User {
     private int id;
     private String name;
     private String email;
-    private String password;
-    private String role;
+    private String passwordHash;
+    private int departmentId;
+    private Role role;
     private Status status;
 
 
-    public User(int id, String name, String email, String password, String role, Status status) {
+    public User(int id, String name, String email, String passwordHash, int departmentId, Role role, Status status) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.password = password;
+        this.passwordHash = passwordHash;
+        this.departmentId = departmentId;
         this.role = role;
         this.status = status;
     }
 
-    // Getters e Setters
+    public User(){
+
+    }
+
     public int getId() {
         return id;
     }
@@ -47,51 +54,59 @@ public class User {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
+    public String getPasswordHash() {
+        return passwordHash;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
     }
 
-    public String getRole() {
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Role getRole() {
         return role;
     }
 
-    public void setRole(String role) {
+    public void setRole(Role role) {
         this.role = role;
     }
 
-    public Status getStatus(){
+    public Status getStatus() {
         return status;
     }
 
-    public void setStatus(Status status){
+    public void setStatus(Status status) {
         this.status = status;
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof User user)) return false;
+        return getId() == user.getId();
+    }
+
+    @Override
     public int hashCode() {
-        return Objects.hash(id, password);
+        return Objects.hashCode(getId());
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        User other = (User) obj;
-        return id == other.id;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
+    public String toString(){
+        return  "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
+                ", passwordHash=" + passwordHash +
+                ", department='" + departmentId + '\'' +
                 ", role='" + role + '\'' +
-                ", status='" + status + '\'' +
+                ", status=" + status +
                 '}';
     }
 }
