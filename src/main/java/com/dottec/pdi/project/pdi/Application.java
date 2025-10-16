8cb0e75c0003a8b41146fe59cc9d9c21a205019c
CauãCursino <cmfcursino@gmail.com>
2025-10-16 11:16:38 -0300

    feat(#10-1): Login and Auth Controller update

--- src/main/java/com/dottec/pdi/project/pdi/Application.java
+++ src/main/java/com/dottec/pdi/project/pdi/Application.java
@@ -19,17 +19,15 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         setDefault(new Locale("pt", "BR"));
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("views/Template.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("views/testeLogin.fxml"));
         Parent root = fxmlLoader.load();
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         Scene scene = new Scene(root, screenBounds.getWidth(), screenBounds.getHeight());
 
-        // Verificar se está correto o local de adição dessa linha de código
         scene.getStylesheets().add(getClass().getResource("/com/dottec/pdi/project/pdi/static/style/style_AddGoal.css").toExternalForm());
-
         stage.setTitle("PDI");
         stage.setScene(scene);
-        stage.setMaximized(true);
+        stage.setMaximized(false);
         stage.show();
 
     }
