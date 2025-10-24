92ff603f0dc27f27cbd5a4192c81f7635d91c895
Breno Reis <reisb6774@gmail.com>
2025-10-24 07:53:02 -0300

    feat(#08-2): Introducing the first lines of code for the DashboardController class

---
+++ src/main/java/com/dottec/pdi/project/pdi/controllers/DashBoardController.java
@@ -0,0 +1,27 @@
+package com.dottec.pdi.project.pdi.controllers  // Refering to the package
+
+public class DashBoardController {
+
+    private final DashboardDAO dashboardDAO = new DashboardDAO();
+
+
+
+    // ------------ Adding the methods right here ----------- //
+
+
+    // ------------- Addition method ---------------- //
+    public void addDashboard(){
+
+    }
+
+    // ------------- Delete method ------------------- //
+    public void deleteDashboard(){
+    }
+
+
+    // --------------- Update method ----------------- //
+    public void updateDashboard(){
+
+    }
+
+}
