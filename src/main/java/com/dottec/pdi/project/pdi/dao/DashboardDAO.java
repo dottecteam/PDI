9c4e187ecd096496d6b6c0c2c822faf3e647f89f
CauãCursino <cmfcursino@gmail.com>
2025-10-24 20:42:23 -0300

    feat(#08-4): Added sector manager DAO and restructured graph logic for each role

--- src/main/java/com/dottec/pdi/project/pdi/dao/DashboardDAO.java
+++ src/main/java/com/dottec/pdi/project/pdi/dao/DashboardDAO.java
@@ -7,10 +7,11 @@ import java.util.List;
 import java.util.Map;
 
 import com.dottec.pdi.project.pdi.config.Database;
+import com.dottec.pdi.project.pdi.controllers.DashboardTagFrequencyController;
 import com.dottec.pdi.project.pdi.enums.ActivityStatus;
 
 public class DashboardDAO {
-
+    
 	private static Connection getConnection() throws SQLException {
 		return Database.getConnection();
 	}
@@ -493,8 +494,8 @@ public class DashboardDAO {
 		}
 		return resultado;
 	}
-
-    public List<DashboardTagFrequencyController> getTopTags() {
+    
+    public static List<DashboardTagFrequencyController> getTopTags() {
         //Lista de dados
         List<DashboardTagFrequencyController> frequencies = new ArrayList<>();
 
@@ -515,6 +516,44 @@ public class DashboardDAO {
 
                 // Adiciona um novo objeto de dados simples à lista
                 frequencies.add(new DashboardTagFrequencyController(nomeTag, contagem));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return frequencies;
+    }
+
+
+
+    //INTERAÇÕES NO BANCO PARA *GERENTE DE SETOR*
+    public static List<DashboardTagFrequencyController> getTopTagsDepartment(int id) {
+        //Lista de dados
+        List<DashboardTagFrequencyController> frequencies = new ArrayList<>();
+
+        //Select na tabela de departments para ver quais tags são mais utilizadas por setor
+        String sql = "SELECT T.tag_name, COUNT(T.tag_id) AS frequencia FROM goal_tags AS GT JOIN goals AS G ON GT.goal_id = G.goa_id JOIN \n" +
+                "collaborators AS C ON G.collaborator_id = C.col_id JOIN tags AS T ON GT.tag_id = T.tag_id WHERE C.department_id = ? GROUP BY \n" +
+                "T.tag_id, T.tag_name ORDER BY frequencia DESC LIMIT 15;";
+
+
+        try (Connection conn = Database.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+
+            //Define o ID do departamento no '?' da query
+            stmt.setInt(1, id);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+
+                // 3. O resto é idêntico: iterar e "traduzir"
+                while (rs.next()) {
+                    String tagName = rs.getString("tag_name");
+                    int frequencia = rs.getInt("frequencia");
+
+                    // 4. Cria o novo "record" de dados
+                    frequencies.add(new DashboardTagFrequencyController(tagName, frequencia));
+                }
             }
 
         } catch (SQLException e) {
